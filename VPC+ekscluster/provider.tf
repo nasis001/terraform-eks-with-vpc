3f3522f90638271a6d0847dfa561b0f74f26a85d
XI3565-Saravanan <106864317+XI3565-Saravanan@users.noreply.github.com>
2022-07-03 15:38:19 +0530

    Update provider.tf

--- VPC+ekscluster/provider.tf
+++ VPC+ekscluster/provider.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-      bucket       = "bucket-pfm"
+    bucket         = "bucket-pfm"
     key            = "terraform/terraform.tfstate"
     region         = "ap-south-1"
     #dynamodb_table = "dynamo-db-table"
